6ade14ca6034264f164509e01782514a4521baf5
ojulien12 <131931473+ojulien12@users.noreply.github.com>
2023-04-30 15:04:11 -0600

    Update 0-format-data.sh

--- Kinetic_parameter_extraction/0-format-data.sh
+++ Kinetic_parameter_extraction/0-format-data.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-# Olivier Julien, 2014
-#ojulien12@gmail.com
 #you will need to format a txt file with 1st column =time and subsequent columns as data (assay readings).Header should be in row 1. Type "last" in the header row of the first empty column after data.  The script is setup to analyze triplicate columns of data for a given group
 #change filename for this script to match you formatted txt file
 # ----------------------------
