913a6175a002598cb29b4fb59fab169efeba18ca
ojulien12 <131931473+ojulien12@users.noreply.github.com>
2023-04-30 15:05:12 -0600

    Update 4-print-stats-2022.sh

--- Kinetic_parameter_extraction/4-print-stats-2022.sh
+++ Kinetic_parameter_extraction/4-print-stats-2022.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-# Olivier Julien, 2014
 # ----------------------------
 ./1-fit-all.sh > temp.txt
 cat temp.txt | egrep -B 18 Theil | egrep -v "Theil|Fitting|Initial|Tolerance|Relative|Computed"
